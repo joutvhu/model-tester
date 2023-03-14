f1976d8ed95a120726535e935a6efd58c510a458
Giao Ho <joutvhu@gmail.com>
2023-03-15 01:53:01 +0700

    Check equals(null)

--- src/main/java/com/joutvhu/model/tester/EqualsTester.java
+++ src/main/java/com/joutvhu/model/tester/EqualsTester.java
@@ -22,14 +22,19 @@ class EqualsTester<T> implements Tester {
     public boolean test() {
         try {
             T model = Creator.anyOf(modelClass).create();
-            boolean success = true;
-            if (safe) {
-                success = Assert.assertEquals(model, model);
-                deepTest(model, null);
-            } else {
+            // Check with itself
+            boolean success = Assert.assertEquals(model, model);
+            // Check equals(null)
+            success = model != null && !model.equals(null) && success;
+            try {
                 T newModel = Creator.makeCopy(model);
-                success = Assert.assertEquals(model, newModel);
+                success = Assert.assertEquals(model, newModel) && success;
                 deepTest(model, newModel);
+            } catch (Throwable x) {
+                if (safe)
+                    deepTest(model, null);
+                else
+                    throw x;
             }
             if (success)
                 System.out.println("Success: " + modelClass.getName() + ".equals()");
@@ -61,7 +66,25 @@ class EqualsTester<T> implements Tester {
                 if (!tested.contains(field) && !Modifier.isFinal(field.getModifiers())) {
                     tested.add(field);
                     field.setAccessible(true);
-                    field.set(newModel, null);
+                    Class<?> fieldType = field.getType();
+                    if (fieldType == boolean.class)
+                        field.set(newModel, false);
+                    else if (fieldType == int.class)
+                        field.set(newModel, 1);
+                    else if (fieldType == long.class)
+                        field.set(newModel, 2l);
+                    else if (fieldType == float.class)
+                        field.set(newModel, 1.1);
+                    else if (fieldType == double.class)
+                        field.set(newModel, 1.2);
+                    else if (fieldType == char.class)
+                        field.set(newModel, 'v');
+                    else if (fieldType == byte.class)
+                        field.set(newModel, (byte) 10);
+                    else if (fieldType == short.class)
+                        field.set(newModel, (byte) 20);
+                    else
+                        field.set(newModel, null);
                     Assert.assertEquals(model, newModel);
                     field.set(newModel, field.get(model));
                 }
